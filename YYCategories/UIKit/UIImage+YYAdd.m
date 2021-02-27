5e48c6058e206ef09885ea76c0f2e1f17598b7e4
aiwalle <wallelj0909@gmail.com>
2021-02-27 17:43:50 +0800

    修改了一下scale

--- YYCategories/UIKit/UIImage+YYAdd.m
+++ YYCategories/UIKit/UIImage+YYAdd.m
@@ -310,7 +310,8 @@ static NSTimeInterval _yy_CGImageSourceGetGIFFrameDelayAtIndex(CGImageSourceRef 
 
 - (UIImage *)imageByResizeToSize:(CGSize)size contentMode:(UIViewContentMode)contentMode {
     if (size.width <= 0 || size.height <= 0) return nil;
-    UIGraphicsBeginImageContextWithOptions(size, NO, self.scale);
+    // liang 这里的scale由self.scale修改为0.0
+    UIGraphicsBeginImageContextWithOptions(size, NO, 0.0);
     [self drawInRect:CGRectMake(0, 0, size.width, size.height) withContentMode:contentMode clipsToBounds:NO];
     UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
     UIGraphicsEndImageContext();
